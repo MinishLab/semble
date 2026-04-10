66ae02c6a24ffd4173723108670b58ef1a935345
Pringled <thomas123@live.nl>
2026-04-10 08:06:14 +0200

    Update docstrings

--- src/semble/index.py
+++ src/semble/index.py
@@ -55,9 +55,9 @@ class SembleIndex:
         """Create and index a SembleIndex from a directory.
 
         :param path: Root directory to index.
-        :param model: Embedding model to use.
-        :param extensions: File extensions to include.
-        :param ignore: Directory names to skip.
+        :param model: Embedding model to use. Defaults to potion-base-16M.
+        :param extensions: File extensions to include. Defaults to a standard set of code extensions.
+        :param ignore: Directory names to skip. Defaults to common VCS and build dirs.
         :param include_docs: If True, also index documentation files (.md, .yaml, etc.).
         :param enable_caching: Whether to persist embeddings to disk between runs.
         :param cache_dir: Override the cache directory. Defaults to ~/.cache/semble.
